39ffd192d67e0de08621b3fbdda6d46ab03bcf6e
Saulo Caliman <saulocalimangomes@gmail.com>
2021-06-21 15:00:44 -0300

    animations player knight

---
+++ Assets/PlayerKnightController.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKnightController : Character
+{
+    [Header("Audio Clips")]
+    public AudioClip hitClip;
+    public AudioClip atkClip;
+
+    [Header("Dash Attributes")]
+    public float dashSpeed;
+    public float dashDelayAnimation;
+    public float dashCooldown;
+
+    float dashCount;
+    float dashCooldownCount;
+
+    bool dashing;
+    float lastH, lastV;
+    float hAxis, vAxis;
+
+    bool isAtk = false;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        dashCooldownCount = dashCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            
+            hAxis = Input.GetAxis("Horizontal");
+            vAxis = Input.GetAxis("Vertical");
+
+            if (dashing == false)
+            {
+                lastH = hAxis;
+                lastV = vAxis;
+            }
+
+            if (Input.GetButton("Fire1"))
+            {
+                myAnimator.SetTrigger("Atk");
+            }
+
+            /*if (Input.GetButton("Fire2") &&
+                dashing == false &&
+                dashCooldownCount >= dashCooldown)
+            {
+                myAnimator.SetBool("Dash", true);
+                gameObject.layer = 12;
+                dashing = true;
+                dashCooldownCount = 0;
+            }
+
+            CountDash();*/
+        }
+    }
+
+    void FixedUpdate() {
+        if (dashing == false)
+        {
+            Move(hAxis, vAxis);
+        }
+        else if (dashing == true)
+        {
+            DoDash();
+        }
+    }
+
+    void Move(float hAxis, float vAxis)
+    {
+        myBody.velocity = speed * new Vector2(hAxis, vAxis).normalized * Time.fixedDeltaTime;
+
+        if (Mathf.Abs(hAxis) >= 0.1)
+        {
+            myAnimator.SetFloat("X", hAxis);
+            myAnimator.SetFloat("Y", 0);
+        }
+        if (Mathf.Abs(vAxis) >= 0.1)
+        {
+            myAnimator.SetFloat("Y", vAxis);
+            myAnimator.SetFloat("X", 0);
+        }
+    }
+
+    void DoDash()
+    {
+        if (lastH == 0 && lastV == 0)
+        {
+            lastH = myAnimator.GetFloat("X");
+            lastV = myAnimator.GetFloat("Y");
+        }
+        Vector2 dir = new Vector2(lastH, lastV).normalized;
+        myBody.velocity = ((speed * dashSpeed) * dir) * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    public void SetAtack()
+    {
+        myAnimator.SetBool("Atk", false);
+    }
+
+    public void DesactiveDash()
+    {
+        dashing = false;
+        myAnimator.SetBool("Dash", false);
+        gameObject.layer = 10;
+    }
+
+    void CountDash()
+    {
+        if (dashing == false)
+        {
+            dashCooldownCount += Time.deltaTime;
+        }
+
+        if (dashing == true)
+        {
+            dashCount = dashCount + Time.deltaTime;
+            if (dashCount >= dashDelayAnimation)
+            {
+                DesactiveDash();
+                dashCount = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "HealthPotion" && currentHealth < maxHealth)
+        {
+            PotionController potion = collision.GetComponent<PotionController>();
+            GainHealth(potion.HealthPoints);
+            Destroy(potion.gameObject);
+        }
+    }
+
+    public override void RecieveDamage(float damage)
+    {
+        base.RecieveDamage(damage);
+        audioSource.PlayOneShot(hitClip);
+    }
+
+    public void PlayAtkSound()
+    {
+        audioSource.PlayOneShot(atkClip);
+    }
+}
